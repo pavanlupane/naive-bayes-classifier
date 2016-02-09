a3b7f6bb760710385c5855723240cbd1d4b4c95b
pavanlupane <pavanlupane@gmail.com>
2016-02-09 13:23:38 -0800

    Final Draft

--- nbclassify3.py
+++ nbclassify3.py
@@ -9,7 +9,7 @@ with open("nbmodel.txt") as jsonFile:
         conditionalProb = json.load(jsonFile)
 
 def calculateProb(token,fileTokens):
-    score = 1
+    score = 1.0
     
     for keyWord in fileTokens:
         if keyWord in  conditionalProb[token]:
@@ -30,11 +30,16 @@ def classifyFileContent(fileTokens,path):
     outString += "" + path
     outFile.write(outString+'\n')
     
+def isNumber(digitString):
+    return digitString.isdigit()
+   
 def processWord(word):
     procWord = "".join(l for l in word if l not in string.punctuation)
     stopWords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours    ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
     procWord = procWord.lower()
     if procWord in stopWords:
+        return 'stop'
+    elif(isNumber(procWord)):
         return 'stop'
     return procWord
 
@@ -52,7 +57,7 @@ def main():
                                 fileTokens.append(myWord)
                 classifyFileContent(fileTokens,path)                    
     
-    print("done")
+    #print("done")
     outFile.close()
                     
 if __name__ == "__main__":main()
--- nblearn3.py
+++ nblearn3.py
@@ -20,16 +20,20 @@ def findCondProb(docClassA,docClassB,token):
                 wordCount = ctr[wordToken]
                 
                 
-                probValue = (wordCount + 1) / (totalWordCount + vocabListLen)
+                probValue = (wordCount + 1.0) / (totalWordCount + vocabListLen)
                 conditionalProb[token][wordToken] = probValue
             else:
                 continue
-                  
+def isNumber(digitString):
+    return digitString.isdigit()
+                 
 def processWord(word):
     procWord = "".join(l for l in word if l not in string.punctuation)
     stopWords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours    ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"]
     procWord = procWord.lower()
     if procWord in stopWords:
+        return 'stop'
+    elif(isNumber(procWord)):
         return 'stop'
     return procWord
               
@@ -88,5 +92,6 @@ def main():
         
     with open('nbmodel.txt','w') as fout:
         json.dump(conditionalProb,fout)
+        
 
 if __name__ == "__main__":main()
